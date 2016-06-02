892a87fa45661f00cdecab8e3921fb8920c34b9e
ericvilleneuve <eric.villeneuve@gmail.com>
2016-06-01 22:28:52 -0400

    add standard error output

--- src/main/java/com/evil/clip/http/Server.java
+++ src/main/java/com/evil/clip/http/Server.java
@@ -1,11 +1,14 @@
 package com.evil.clip.http;
 
 import com.vtence.molecule.WebServer;
+import com.vtence.molecule.middlewares.FailureMonitor;
 import com.vtence.molecule.routing.DynamicRoutes;
 import com.vtence.molecule.servers.SimpleServer;
 
 import java.io.IOException;
 import java.net.URI;
+
+import static com.evil.clip.http.middleware.PlainErrorReporter.toStandardError;
 
 public class Server {
 
@@ -17,6 +20,7 @@ public class Server {
 
     public void start() throws IOException {
 
+        server.add(new FailureMonitor(toStandardError()));
         server.start(new DynamicRoutes() {{
             get("/").to(new LandingController());
         }});
