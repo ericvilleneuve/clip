09b2e048bd311dae7df11e9d43508237fde55fbe
ericvilleneuve <eric.villeneuve@gmail.com>
2016-06-03 18:19:17 -0400

    add a ExpandController which fetches from repository and returns original url.

---
+++ src/main/java/com/evil/clip/http/ExpandController.java
@@ -0,0 +1,23 @@
+package com.evil.clip.http;
+
+import com.evil.clip.domain.UrlRepository;
+import com.vtence.molecule.Application;
+import com.vtence.molecule.Request;
+import com.vtence.molecule.Response;
+import com.vtence.molecule.http.HttpStatus;
+
+public class ExpandController implements Application {
+
+    private UrlRepository urlRepository;
+
+    public ExpandController(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @Override
+    public void handle(Request request, Response response) throws Exception {
+        String originalUrl = urlRepository.findByShortUrl(request.parameter("url"));
+
+        response.status(HttpStatus.OK).body(originalUrl).done();
+    }
+}
---
+++ src/test/java/com/evil/clip/http/ExpandControllerTest.java
@@ -0,0 +1,50 @@
+package com.evil.clip.http;
+
+import com.evil.clip.domain.UrlRepository;
+import com.vtence.molecule.Request;
+import com.vtence.molecule.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.vtence.molecule.testing.BodyContent.asText;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ExpandControllerTest {
+
+    ExpandController controller;
+    private Response response = new Response();
+    private Request request = new Request();
+    private UrlRepository urlRepository;
+
+    @Before
+    public void thisController() throws Exception {
+        urlRepository = mock(UrlRepository.class);
+        controller = new ExpandController(urlRepository);
+    }
+
+    @Test
+    public void returnsOk() throws Exception {
+        controller.handle(request, response);
+        assertThat(response.statusCode(), is(200));
+    }
+
+    @Test
+    public void retrievesOriginalUrlFromShortened() throws Exception {
+        request.addParameter("url", "http://short.url");
+        controller.handle(request, response);
+
+        verify(urlRepository).findByShortUrl("http://short.url");
+    }
+
+    @Test
+    public void returnsOriginalUrlWhenItExists() throws Exception {
+        request.addParameter("url", "http://short.url");
+
+        when(urlRepository.findByShortUrl("http://short.url")).thenReturn("http://original-long.url");
+        controller.handle(request, response);
+
+        assertThat(asText(response), is("http://original-long.url"));
+    }
+}
